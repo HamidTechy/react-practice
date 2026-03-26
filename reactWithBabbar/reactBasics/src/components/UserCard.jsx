42696b55f995b6699c06a935139ab53cb50097db
Hamid Rana <ranahamid728@gmail.com>
2026-03-26 15:37:56 +0500

    added html content in componentand now styling of that component

--- reactWithBabbar/reactBasics/src/components/UserCard.jsx
+++ reactWithBabbar/reactBasics/src/components/UserCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import HamidPic from '../assets/HamidPic.jpg'
 const UserCard = () => {
   return (
-    <div>
-      
+    <div className='user-container'>
+      <p id="user-name">Hamid Rana</p>
+      <img src={HamidPic} alt="Hamid" id="user-img" />
+      <p id="user-desc">Description of Hamid Rana</p>
     </div>
   )
 }
--- reactWithBabbar/reactBasics/src/main.jsx
+++ reactWithBabbar/reactBasics/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import UserCard from './components/UserCard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <UserCard />
+    <UserCard />
+    <UserCard />
   </StrictMode>
   
 )
